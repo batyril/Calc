67c2d0d088472fb24fef05a62aba1ca477d0a872
batyril <237x237@gmail.ru>
2022-01-15 03:17:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+import {UI_ELEMETS} from "../js/view.js";
+
+let valueA = '';
+let valueB = '';
+let operand = '';
+
+
+UI_ELEMETS.CLEAR.addEventListener("click",function(){
+    valueA = '';
+    valueB = '';
+    operand = '';   
+    UI_ELEMETS.OUTPUT.textContent = '0'
+})
+
+UI_ELEMETS.CALCULATOR.onclick = function(event) {
+    let isButtons = event.target.textContent === 'C' || event.target.textContent === '=' || event.target.className === 'delete' || event.target.className === 'calculator_block' || event.target.className === 'row row-1' || event.target.className === 'row row-2' || event.target.className === 'row row-3' || event.target.className === 'row row-4'  || event.target.className === 'row row-5' || event.target.className === 'row row-6' || event.target.className === 'output' ;
+    if(isButtons){
+        return
+    };
+    
+    if(UI_ELEMETS.OUTPUT.textContent ==='0'){
+        UI_ELEMETS.OUTPUT.textContent = '';
+        UI_ELEMETS.OUTPUT.textContent = event.target.textContent;
+    }
+    else{
+        UI_ELEMETS.OUTPUT.textContent += event.target.textContent};}
+    
+    
+    
+
+UI_ELEMETS.DELETE.addEventListener("click",function(){
+
+    if(UI_ELEMETS.OUTPUT.textContent.length >= 1){
+        UI_ELEMETS.OUTPUT.textContent = UI_ELEMETS.OUTPUT.textContent.slice(0,-1)
+    }
+
+    if(UI_ELEMETS.OUTPUT.textContent ==="" || UI_ELEMETS.OUTPUT.textContent === "0" ){
+        UI_ELEMETS.OUTPUT.textContent = '0';
+        return
+    }  
+})
+
+UI_ELEMETS.EQUALLY.addEventListener("click",function(){
+    let sumA ='';
+    let sumB ='';
+    for (let char of  UI_ELEMETS.OUTPUT.textContent){
+
+        if(char ==='÷' || char ==='×' || char ==='-'|| char ==='+'){
+            operand = char 
+            break
+        }
+        sumA += char 
+    }
+    valueA = +sumA
+    for (let char of  UI_ELEMETS.OUTPUT.textContent.split('').reverse().join('')){
+        if(char ==='÷' || char ==='×' || char ==='-'|| char ==='+'){
+            break
+        }
+        sumB += char  
+
+    }
+    
+    valueB = +sumB.split('').reverse().join('')
+   
+    calc(valueA,valueB, operand )
+
+})
+
+
+
+function calc(a, b, operator) {
+    let result;
+    console.log('+')
+    const isNotValidOperand = ( typeof a != 'number' || typeof b != 'number' );
+    
+    if ( isNotValidOperand ) {
+        result = "Error";
+    } else if (operator === "+") {
+        result = a + b;
+    } else if (operator === "-") {
+        result = a - b; 
+    } else if (operator === "×") {
+        result = a * b;
+    } else if (operator === "÷") {
+        if (b === 0) { 
+            result = "Error!";
+        } else {
+            result = a / b;
+        } 
+    }  else {
+        result = "unknown";
+    }
+
+    UI_ELEMETS.OUTPUT.textContent = result;
+}
+
+
+
+
+
---
+++ js/view.js
@@ -0,0 +1,11 @@
+export const UI_ELEMETS = {
+    CLEAR: document.querySelector('.clear'),
+    MULTI: document.querySelector('.multiplication'),
+    MINUS: document.querySelector('.minus'),
+    PLUS : document.querySelector('.plus'),
+    EQUALLY : document.querySelector('.equally'),
+    NUMBER : document.querySelectorAll('.number'),
+    OUTPUT :  document.querySelector('.output'),
+    CALCULATOR : document.querySelector('.calculator_block'),
+    DELETE: document.querySelector('.delete'),
+}
